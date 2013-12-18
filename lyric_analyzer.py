f43626d11acd9ac61612f32dac7bed11ed179af7
otknoy <otknoy@gmail.com>
2013-12-19 00:53:35 +0900

    calculate tf and tfidf

--- lyric_analyzer.py
+++ lyric_analyzer.py
@@ -1,55 +1,59 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 import sys
 path="../nlp"
 sys.path.append(path)
-
 import nlp
-import k_means
-
-def extract_lyric(s):
-    return s.split('lyric:\n')[-1]
 
 
+def load_lyric_file(filename):
+    f = open(filename)
+    text = nlp.normalize(f.read().decode('utf-8'))
+    f.close()
+    return ['\n'.join(e.split('\n')[1:]) for e in text.split('\n\n')]
+
+    
 if __name__ == '__main__':
-    filenames = sys.argv[1:]
-
-    lyrics = []
-    for filename in filenames:
-        f = open(filename)
-        lyric = nlp.normalize(extract_lyric(f.read()).decode('utf-8'))
-        f.close()
-        lyrics.append(lyric)
+    import argparse
+    parser = argparse.ArgumentParser(description='lyric analyzer')
+    parser.add_argument('infile', nargs='*')
+    args = parser.parse_args()
+    filenames = args.infile
 
     docs = []
-    for lyric in lyrics:
-        doc = nlp.mecab(lyric.encode('utf-8'))
-        docs.append(doc.select_noun())
-
-    text_collection = nlp.TextCollection(docs)
-    tf = text_collection.tf
-    tfidf = text_collection.tfidf
-
-
-    # output tf of text collection
-    # for t, f in sorted(tf.items(), key=lambda x:x[1]):
-    #     print t, f
-
     for filename in filenames:
-        print filename + '.tf'
+       title, singer, writer, composer, year, sex, lyric = load_lyric_file(filename)
+       terms = nlp.tokenizer(nlp.normalize(lyric))
+       terms = nlp.extract_noun(terms)
+       terms = nlp.remove_stopword(terms)
+       # print ' '.join([t.basic_form for t in terms])
+       docs.append([t.basic_form for t in terms] )
+
+       
+    # all tf
+    print 'all tf'
+    from itertools import chain
+    all_doc = list(chain.from_iterable(docs))
+    tf = nlp.term_frequency(all_doc)
+    for t, f in sorted(tf.items(), key=lambda x:-x[1])[:10]:
+        print "%s, %d" % (t.encode('utf-8'), f)
+    print
         
-    
-    
-    ## output term-document matrix
-    # print 'term, ',
-    # print ', '.join([f.split('/')[-1] for f in filenames])
 
-    # for df in text_collection.df.keys():
-    #     print df + ', ',
-    #     print ', '.join([str(tfidf[df]) for tfidf in text_collection.tfidf])
+    # tf
+    print 'tf'
+    for d in docs:
+        print filenames[docs.index(d)]
+        tf = nlp.term_frequency(d)
+        for t, f in sorted(tf.items(), key=lambda x:-x[1])[:10]:
+            print "%s, %d" % (t.encode('utf-8'), f)
+    print
+            
 
-    # for tfidf in text_collection.tfidf:
-    #     print ', '.join([str(v) for v in tfidf.values()])
-
-
-    
+    # tfidf
+    print 'tfidf'
+    tfidf_list = nlp.tf_idf(docs)
+    for tfidf in tfidf_list:
+        print filenames[tfidf_list.index(tfidf)]
+        for t, w in sorted(tfidf.items(), key=lambda x:-x[1])[:10]:
+            print "%s, %f" % (t.encode('utf-8'), w)
