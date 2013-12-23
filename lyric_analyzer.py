0b8b906d27212e5cfa1772665a23fac6585811ca
otknoy <otknoy@gmail.com>
2013-12-23 21:49:20 +0900

    year and sex classification

--- lyric_analyzer.py
+++ lyric_analyzer.py
@@ -7,6 +7,7 @@ import nlp
 
 import re
 
+
 def load_lyric_file(filename):
     f = open(filename)
     text = nlp.normalize(f.read().decode('utf-8'))
@@ -18,26 +19,46 @@ def load_lyric_file(filename):
         ret[label] = s
     return ret
 
-def classify_by_year(info):
+def classify_by_year(songs, month=False):
     ret = {}
-    for i in info:
-        y, m, d = i['year'].split('/')
-        key = y + '/' + m
+    for s in songs:
+        y, m, d = s['year'].split('/')
+        key = y
+        if month: key += '/' + m
         if not ret.has_key(key):
             ret[key] = []
-        ret[key].append(i)
+        ret[key].append(s)
     return ret
 
-def classify_by_sex(info):
+def classify_by_sex(songs):
     ret = {}
-    for i in info:
-        sex = i['sex']
-        if not ret.has_key(sex):
-            ret[sex] = []
-        ret[sex].append(i)
+    for s in songs:
+        key = s['sex']
+        if not ret.has_key(key):
+            ret[key] = []
+        ret[key].append(s)
     return ret
-    
 
+
+def lsa(matrix, dim):
+    import sklearn.decomposition
+    import sklearn.preprocessing
+    lsa = sklearn.decomposition.TruncatedSVD(dim)
+    matrix = lsa.fit_transform(matrix)
+    matrix = sklearn.preprocessing.Normalizer(copy=False).fit_transform(matrix)
+    return matrix
+
+def kmeans(matrix, k=10):
+    import sklearn.cluster
+    km  = sklearn.cluster.KMeans(n_clusters=k, init='k-means++', n_init=1, verbose=True)
+    km.fit(matrix).labels
+    return km
+    # labels = km.labels_
+    # print labels
+    # for f, l in sorted(zip(filenames, labels), key=lambda x:x[1]):
+    #     print l, f.split('/')[-1].split('.')[0]
+
+                
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='lyric analyzer')
@@ -50,47 +71,30 @@ if __name__ == '__main__':
         song = load_lyric_file(filename)
         songs.append(song)
 
-    by_year = classify_by_year(songs)
+    by_year = classify_by_year(songs, month=False)
     by_sex = classify_by_sex(songs)
 
-    docs_year = {}
-    for year, d in sorted(by_year.items()):
-        lyric = '\n'.join([e['lyric'] for e in d])
-        lyric = nlp.normalize(lyric)
-        terms = nlp.tokenizer(lyric)
-        terms = nlp.extract_noun(terms)
-        terms = nlp.remove_stopword(terms)
-        docs_year[year] = [t.basic_form for t in terms]
+    # print by_year.keys()
+    # print by_sex.keys()
 
-    for year, d in sorted(docs_year.items()):
-        print "%s, %d" % (year, len(by_year[year]))
-        # tf = nlp.term_frequency(d)
-        # for t, f in sorted(tf.items(), key=lambda x:-x[1]):
-        #     print t.encode('utf-8'), f
-        # print 
-        
-
-
-    exit()
-
-    
     docs = []
-    male = []
-    female = []
-    for d in data:
-        terms = nlp.tokenizer(nlp.normalize(d['lyric']))
+    for sex, songs in by_year.items():
+        text = '\n'.join([s['lyric'] for s in songs]) 
+        text = nlp.normalize(text)
+        terms = nlp.tokenizer(text)
         terms = nlp.extract_noun(terms)
         terms = nlp.remove_stopword(terms)
-        if d['sex'] == 'male':
-            male.append([t.basic_form for t in terms])
-        elif d['sex'] == 'female':
-            female.append([t.basic_form for t in terms])
         docs.append([t.basic_form for t in terms])
 
-    # print len(filenames)
-    # print len(male)
-    # print len(female)
-       
+    # tf
+    for label, doc in sorted(zip(by_year.keys(), docs), key=lambda x:x[0]):
+        print label
+        tf = nlp.term_frequency(doc, normalize=True)
+        for t, w, in sorted(tf.items(), key=lambda x:-x[1])[:10]:
+            print t.encode('utf-8'), w
+        print
+
+    
     # all tf
     from itertools import chain
     all_doc = list(chain.from_iterable(docs))
@@ -100,47 +104,14 @@ if __name__ == '__main__':
         print "%s, %d" % (t.encode('utf-8'), f)
     print
 
-    exit()
-
-    # tf
-    # print 'tf'
-    # for d in docs:
-    #     print filenames[docs.index(d)]
-    #     tf = nlp.term_frequency(d)
-    #     for t, f in sorted(tf.items(), key=lambda x:-x[1])[:10]:
-    #         print "%s, %d" % (t.encode('utf-8'), f)
-    # print
-            
-
-    # tfidf
-    print 'tfidf'
-    tfidf_list = nlp.tf_idf(docs)
-    for tfidf in tfidf_list:
-        print filenames[tfidf_list.index(tfidf)]
-        for t, w in sorted(tfidf.items(), key=lambda x:-x[1])[:10]:
-            print "%s, %f" % (t.encode('utf-8'), w)
-
-
-    exit()
     
-    # kmeans
-    import numpy
-    matrix = numpy.array([e.values() for e in tfidf_list])
-    import sklearn.decomposition
-    import sklearn.preprocessing
-    dim = 20
-    lsa = sklearn.decomposition.TruncatedSVD(dim)
-    matrix = lsa.fit_transform(matrix)
-    matrix = sklearn.preprocessing.Normalizer(copy=False).fit_transform(matrix)
 
-    import sklearn.cluster
-    k = 30
-    km  = sklearn.cluster.KMeans(n_clusters=k, init='k-means++', n_init=1, verbose=True)
-    km.fit(matrix)
+    import numpy 
+    matrix = numpy.array([e.values() for e in corpus])
+    matrix = lsa(matrix, dim=20)
+    km = kmeans(matrix, k=10)
+
     labels = km.labels_
-
     print labels
     for f, l in sorted(zip(filenames, labels), key=lambda x:x[1]):
         print l, f.split('/')[-1].split('.')[0]
-    
-            
